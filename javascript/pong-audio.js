d46c15751a026f2433d1fb282f9d83f292b0d91d
RogerBrook <154286608+RogerBrook@users.noreply.github.com>
2024-01-04 17:30:10 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -46,7 +46,7 @@ export var soundArray = []; //list of sounds loaded
 export var wallSound = new soundFile("wall.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
-export var paddleSound = new soundFile("woosh.wav");
+export var paddleSound = new soundFile("throwknife2.mp3");
 soundArray.push(paddleSound);
 
 export var scoreSound = new soundFile("silence.mp3");
